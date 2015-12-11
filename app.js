9e61e6ee639a61e94b0f7e239d9594f788ab5431
Travis Johnston <tjohnst1@gmail.com>
2015-12-10 22:12:53 -0800

    create the basis for adding and editing bookmarks; create clear categories functionality

--- app.js
+++ app.js
@@ -18,14 +18,47 @@ app.controller('MainCtrl', function(){
     {"id": 5, "title": "FIFA", "url": "http://angular.js", "category": "Soccer"}
   ];
 
+  // Categories //
   this.currentCategory = "";
-
   this.setCategory = function(category){
     this.currentCategory = category;
   };
-
   this.isCurrentCategory = function(category){
     return (this.currentCategory === category) && (this.currentCategory !== "")
   }
+  this.clearCategories = function(){
+    this.currentCategory = "";
+  }
+
+  // Editing & Creating Bookmarks //
+  this.creatingBookmark = false;
+  this.editingBookmark = false;
+  this.createNew = true;
+
+  this.startCreatingBookmark = function(){
+    this.creatingBookmark = true;
+    this.editingBookmark = false;
+    this.createNew = false;
+  };
+
+  this.startEditingBookmark = function(){
+    this.creatingBookmark = false;
+    this.editingBookmark = true;
+    this.createNew = false;
+  };
+
+  this.shouldShowCreating = function(){
+    return this.currentCategory && (this.creatingBookmark === true)
+  };
+
+  this.shouldShowEditing = function(){
+    return this.currentCategory && (this.editingBookmark === true)
+  };
+
+  this.cancelForm = function(){
+    this.creatingBookmark = false;
+    this.editingBookmark = false;
+    this.createNew = true;
+  };
 
 });
